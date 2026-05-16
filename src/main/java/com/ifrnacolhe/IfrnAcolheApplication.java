0247f2828da37d9d990aa9a7ccb0201fe5f1fa62
Jadson Teixeira <jadsontxm@gmail.com>
2026-05-16 15:18:42 -0300

    iniciando o repositório

---
+++ src/main/java/com/ifrnacolhe/IfrnAcolheApplication.java
@@ -0,0 +1,13 @@
+package com.ifrnacolhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfrnAcolheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfrnAcolheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifrnacolhe/IfrnAcolheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifrnacolhe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfrnAcolheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
