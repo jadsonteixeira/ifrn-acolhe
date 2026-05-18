bc35c3d7b08541fdd0fecfb5a827871a30106d5a
Jadson Teixeira <jadsontxm@gmail.com>
2026-05-18 16:18:12 -0300

    adicionado exceptions, verificação de nome do sentimento duplicado e doc no Swagger

--- src/main/java/com/ifrnacolhe/business/SentimentoService.java
+++ src/main/java/com/ifrnacolhe/business/SentimentoService.java
@@ -4,6 +4,7 @@ import com.ifrnacolhe.business.dto.request.SentimentoRequestDTO;
 import com.ifrnacolhe.business.dto.response.SentimentoResponseDTO;
 import com.ifrnacolhe.business.mapper.SentimentoMapper;
 import com.ifrnacolhe.infrastructure.entity.Sentimento;
+import com.ifrnacolhe.infrastructure.exceptions.ConflictException;
 import com.ifrnacolhe.infrastructure.repository.SentimentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,10 +18,18 @@ public class SentimentoService {
 
     public SentimentoResponseDTO salvar(SentimentoRequestDTO dto) {
 
+        verificarSentimentoExistente(dto.getNome());
+
         Sentimento entity = sentimentoMapper.toEntity(dto);
 
         entity.ativar();
 
         return sentimentoMapper.toResponseDTO(sentimentoRepository.save(entity));
+    }
+
+    private void verificarSentimentoExistente(String nome) {
+        if (sentimentoRepository.existsByNome(nome)) {
+            throw new ConflictException("Já existe um sentimento cadastrado com esse nome! - Nome: " + nome);
+        }
     }
 }
--- src/main/java/com/ifrnacolhe/controller/SentimentoController.java
+++ src/main/java/com/ifrnacolhe/controller/SentimentoController.java
@@ -3,7 +3,11 @@ package com.ifrnacolhe.controller;
 import com.ifrnacolhe.business.SentimentoService;
 import com.ifrnacolhe.business.dto.request.SentimentoRequestDTO;
 import com.ifrnacolhe.business.dto.response.SentimentoResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ifrnacolhe/sentimentos")
 @RequiredArgsConstructor
+@Tag(name = "Sentimento", description = "CRUD de Sentimento")
 public class SentimentoController {
 
     private final SentimentoService sentimentoService;
 
     @PostMapping
+    @Operation(summary = "Cadastrar Sentimento", description = "Cria um novo sentimento")
+    @ApiResponse(responseCode = "201", description = "Sentimento cadastrado com sucesso")
+    @ApiResponse(responseCode = "409", description = "Já existe um sentimento cadastrado com esse nome")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<SentimentoResponseDTO> salvar(@RequestBody SentimentoRequestDTO dto) {
-        return ResponseEntity.ok(sentimentoService.salvar(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(sentimentoService.salvar(dto));
     }
 }
